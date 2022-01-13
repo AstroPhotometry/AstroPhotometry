98153d0f6e161481f049d781a85440c508354b62
ishay320 <ishay320@hotmail.com>
2022-01-13 14:54:12 +0200

    open files with with
    close #2

--- FitsMath.py
+++ FitsMath.py
@@ -35,68 +35,70 @@ def main(argv: list[str]):
     output_file_name: str = argv[4]
 
     # open the files TODO: check if file exists
-    first_file: fits.hdu.hdulist.HDUList = fits.open(first_file_name, mode='readonly')
-    second_file: fits.hdu.hdulist.HDUList = fits.open(second_file_name, mode='readonly')
+    with fits.open(first_file_name, mode='readonly') as first_file, fits.open(
+            second_file_name, mode='readonly') as second_file:
+        # first_file: fits.hdu.hdulist.HDUList = fits.open(first_file_name, mode='readonly')
+        # second_file: fits.hdu.hdulist.HDUList = fits.open(second_file_name, mode='readonly')
 
-    operator = ""  # the operator to write into the header
+        operator = ""  # the operator to write into the header
 
-    if operand == "-A":
-        operator = "avarage"
-        out_picture = first_file[0].data[:, :]
-        out_picture += second_file[0].data[:, :]
-        out_picture = out_picture[:, :] / 2
-    elif operand == "-m":
-        operator = "minus"
-        out_picture = first_file[0].data[:, :]
-        out_picture -= second_file[0].data[:, :]
-    elif operand == "-M":
-        operator = "multiplication"
-        out_picture = first_file[0].data[:, :]
-        out_picture *= second_file[0].data[:, :]
-    elif operand == "-a":
-        operator = "addition"
-        out_picture = first_file[0].data[:, :]
-        out_picture += second_file[0].data[:, :]
-    elif operand == "-d":  # TODO: check for 0
-        operator = "division"
-        out_picture = first_file[0].data[:, :]
-        out_picture = out_picture / second_file[0].data[:, :]
-    # elif operand[0] == '-' and operand[1:].isdigit(): # unary operand
-    #     num: int = int(operand[1:])
-    #     if num == 0:
-    #         print("ERROR: can not divide by 0")
-    #         print_usage(argv[0])
-    #         exit(1)
-    #     out_picture = first_file[0].data[:, :]
-    #     out_picture = out_picture / num
-    else:
-        first_file.close()
-        second_file.close()
-        print_usage(argv[0])
-        exit(1)
+        if operand == "-A":
+            operator = "avarage"
+            out_picture = first_file[0].data[:, :]
+            out_picture += second_file[0].data[:, :]
+            out_picture = out_picture[:, :] / 2
+        elif operand == "-m":
+            operator = "minus"
+            out_picture = first_file[0].data[:, :]
+            out_picture -= second_file[0].data[:, :]
+        elif operand == "-M":
+            operator = "multiplication"
+            out_picture = first_file[0].data[:, :]
+            out_picture *= second_file[0].data[:, :]
+        elif operand == "-a":
+            operator = "addition"
+            out_picture = first_file[0].data[:, :]
+            out_picture += second_file[0].data[:, :]
+        elif operand == "-d":  # TODO: check for 0
+            operator = "division"
+            out_picture = first_file[0].data[:, :]
+            out_picture = out_picture / second_file[0].data[:, :]
+        # elif operand[0] == '-' and operand[1:].isdigit(): # unary operand
+        #     num: int = int(operand[1:])
+        #     if num == 0:
+        #         print("ERROR: can not divide by 0")
+        #         print_usage(argv[0])
+        #         exit(1)
+        #     out_picture = first_file[0].data[:, :]
+        #     out_picture = out_picture / num
+        else:
+            first_file.close()
+            second_file.close()
+            print_usage(argv[0])
+            exit(1)
 
-    if output_file_name == "-s":
-        import matplotlib.pyplot as plt
-        from astropy.visualization import astropy_mpl_style
-        plt.style.use(astropy_mpl_style)
-        plt.figure()
-        plt.grid(False)
-        plt.imshow(out_picture, cmap='gray')
-        plt.colorbar()
-        plt.show()
-    else:
-        hdr = fits.Header()
-        import datetime
-        date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
-        hdr['history'] = f"= edited on {date}"
-        hdr['COMMENT'] = f"= this is an {operator} of 2 fits files"
-        hdu = fits.PrimaryHDU(data=out_picture, header=hdr)
+        if output_file_name == "-s":
+            import matplotlib.pyplot as plt
+            from astropy.visualization import astropy_mpl_style
+            plt.style.use(astropy_mpl_style)
+            plt.figure()
+            plt.grid(False)
+            plt.imshow(out_picture, cmap='gray')
+            plt.colorbar()
+            plt.show()
+        else:
+            hdr = fits.Header()
+            import datetime
+            date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
+            hdr['history'] = f"= edited on {date}"
+            hdr['COMMENT'] = f"= this is an {operator} of 2 fits files"
+            hdu = fits.PrimaryHDU(data=out_picture, header=hdr)
 
-        hdul = fits.HDUList([hdu])
-        hdul.writeto(output_file_name, overwrite=True)
+            hdul = fits.HDUList([hdu])
+            hdul.writeto(output_file_name, overwrite=True)
 
-    first_file.close()
-    second_file.close()
+    # first_file.close()
+    # second_file.close()
 
 
 if __name__ == "__main__":
