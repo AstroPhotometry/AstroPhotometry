d09207f5f7c26394ee77c8944578f827e66caa14
ishay320 <ishay320@hotmail.com>
2022-02-01 12:40:35 +0200

    added pipping to showFits

--- showfits.py
+++ showfits.py
@@ -8,10 +8,13 @@ from astropy.io import fits
 import sys
 
 
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
 # TODO: add -s show only , -c colors
 def print_usage(file_name: str) -> None:
     split_filename = file_name.split(sep="\\")
-    print(f"usage: {split_filename[-1]} <file> optional: <output file>")
+    eprint(f"usage: {split_filename[-1]} <file or -p for piping> optional: <png output file>")
 
 
 def main(argv: list[str]):
@@ -19,7 +22,17 @@ def main(argv: list[str]):
         print_usage(argv[0])
         exit(1)
 
-    first_file = fits.open(argv[1].replace('/', '\\'), mode='readonly')
+    if "-p" in argv:
+        argv.remove("-p")
+        input_file = input()
+        input_file = input_file.removeprefix("\ufeff")
+        if len(input_file) == 0:
+            eprint("PIPING ERROR: did not received file name")
+            exit(1)
+    else:
+        input_file = argv[-1]
+
+    first_file = fits.open(input_file.replace('/', '\\'), mode='readonly')
 
     first_file.info()
 
@@ -30,7 +43,7 @@ def main(argv: list[str]):
     plt.colorbar()
     plt.grid(False)
 
-    if len(argv) > 2:
+    if len(argv) > 1:
         plt.savefig(argv[2], format='png', bbox_inches='tight', dpi=80)
     plt.show()
 
