688087b8454f2be5fa9fe79f32783d2bed2400fc
Uriya Shalom <uriyas10@gmail.com>
2022-03-22 23:16:37 +0200

    addad test

---
+++ test/test/VM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    public class VM
+    {
+        public string Title { get; set; }
+        public Mycommand ShowCommand { get; set; }
+
+        public VM()
+        {
+            ShowCommand = new Mycommand();
+            Title = "ariel";
+        }
+
+    }
+}
