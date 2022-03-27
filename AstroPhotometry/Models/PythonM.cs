06289c10b4865d3532ee81cd6efee1785c672cb8
Uriya Shalom <uriyas10@gmail.com>
2022-03-27 19:45:43 +0300

    run python from cs

--- AstroPhotometry/Models/PythonM.cs
+++ AstroPhotometry/Models/PythonM.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace AstroPhotometry.Models
 {
-    internal class PythonM
+    public class PythonM
     {
+        public void run()
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "C:/Users/1/PycharmProjects/pythonProject1/src/hello.py";
+            //startInfo.Arguments = "/C copy /b Image1.jpg + Archive.rar Image2.jpg";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
     }
+    
 }
