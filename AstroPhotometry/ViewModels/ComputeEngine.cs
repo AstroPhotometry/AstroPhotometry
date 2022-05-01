4a95cc52ff4a55b348032eb2660da66b06d9d1ad
Uriya Shalom <uriyas10@gmail.com>
2022-05-01 20:37:48 +0300

    Add classes for compution editing

---
+++ AstroPhotometry/ViewModels/ComputeEngine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstroPhotometry.ViewModels
+{
+    public class ComputeEngine
+    {
+        // func to compute all the nodes
+    }
+}
