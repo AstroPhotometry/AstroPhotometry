97f125529854d113f43fdaf8a8729985da02511c
Uriya Shalom <74024383+uriya222@users.noreply.github.com>
2022-05-01 19:21:43 +0300

    Update PhotoM.cs

--- AstroPhotometry/Models/PhotoM.cs
+++ AstroPhotometry/Models/PhotoM.cs
@@ -21,11 +21,11 @@ namespace AstroPhotometry.Models
         public Uri Path
         {
             get { return uri; }
+            set { uri = value; }
         }
         public BitmapImage ImageUri
         {
             get { return image; }
-            set { image = value; }
         }
     }
 }
