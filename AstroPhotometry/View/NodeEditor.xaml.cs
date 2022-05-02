5a87c828dee09bc3d00eefbdb621ff3fb0a86845
ishay320 <ishay320@hotmail.com>
2022-05-02 22:49:34 +0300

    all the buttons in place

--- AstroPhotometry/View/NodeEditor.xaml.cs
+++ AstroPhotometry/View/NodeEditor.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,72 @@ namespace AstroPhotometry.View
     /// </summary>
     public partial class NodeEditor : UserControl
     {
+        private string[] bias;
+        private string[] dark;
+        private string[] flat;
+        private string[] light;
+        
         public NodeEditor()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click_Bias(object sender, RoutedEventArgs e)
+        {
+            bias = fileDialog();
+        }
+
+        private void Button_Click_Dark(object sender, RoutedEventArgs e)
+        {
+            dark = fileDialog();
+
+        }
+
+        private void Button_Click_Flat(object sender, RoutedEventArgs e)
+        {
+            flat = fileDialog();
+
+        }
+
+        private void Button_Click_Light(object sender, RoutedEventArgs e)
+        {
+            light = fileDialog();
+
+        }
+
+        private void Button_Click_Run(object sender, RoutedEventArgs e)
+        {
+            if (bias != null && dark != null && flat != null && light != null)
+            {
+                MessageBox.Show("running");
+            }
+            else
+            {
+                MessageBox.Show("somthing missing");
+            }
+        }
+
+        private string[] fileDialog()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Fits files (*.fits)|*.fit;*.fits|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // debug
+                string txt = "";
+                foreach (string file in openFileDialog.FileNames)
+                {
+                    txt += " " + file;
+                }
+                MessageBox.Show(txt);
+                //
+                return openFileDialog.FileNames;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
