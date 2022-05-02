745a5d8e4467cf0dfb87656ba35da499e0162951
ishay320 <ishay320@hotmail.com>
2022-05-02 23:12:52 +0300

    show the names on screen

--- AstroPhotometry/View/NodeEditor.xaml.cs
+++ AstroPhotometry/View/NodeEditor.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AstroPhotometry.View
 {
@@ -25,7 +14,9 @@ namespace AstroPhotometry.View
         private string[] dark;
         private string[] flat;
         private string[] light;
-        
+
+        private const int show_max = 2; // Max files to show
+
         public NodeEditor()
         {
             InitializeComponent();
@@ -34,24 +25,81 @@ namespace AstroPhotometry.View
         private void Button_Click_Bias(object sender, RoutedEventArgs e)
         {
             bias = fileDialog();
+
+            if (bias != null)
+            {
+                string tmp = Path.GetFileName(bias[0]);
+                for (int i = 1; i < bias.Length && i < show_max; i++)
+                {
+                    tmp += ", " + Path.GetFileName(bias[i]);
+                }
+                if (bias.Length > show_max)
+                {
+                    tmp += " ...";
+                }
+
+                bias_text.Text = tmp;
+            }
         }
 
         private void Button_Click_Dark(object sender, RoutedEventArgs e)
         {
             dark = fileDialog();
 
+            if (dark != null)
+            {
+                string tmp = Path.GetFileName(dark[0]);
+                for (int i = 1; i < dark.Length && i < show_max; i++)
+                {
+                    tmp += ", " + Path.GetFileName(dark[i]);
+                }
+                if (dark.Length > show_max)
+                {
+                    tmp += " ...";
+                }
+
+                dark_text.Text = tmp;
+            }
         }
 
         private void Button_Click_Flat(object sender, RoutedEventArgs e)
         {
             flat = fileDialog();
 
+            if (flat != null)
+            {
+                string tmp = Path.GetFileName(flat[0]);
+                for (int i = 1; i < flat.Length && i < show_max; i++)
+                {
+                    tmp += ", " + Path.GetFileName(flat[i]);
+                }
+                if (flat.Length > show_max)
+                {
+                    tmp += " ...";
+                }
+
+                flat_text.Text = tmp;
+            }
         }
 
         private void Button_Click_Light(object sender, RoutedEventArgs e)
         {
             light = fileDialog();
 
+            if (light != null)
+            {
+                string tmp = Path.GetFileName(light[0]);
+                for (int i = 1; i < light.Length && i < show_max; i++)
+                {
+                    tmp += ", " + Path.GetFileName(light[i]);
+                }
+                if (light.Length > show_max)
+                {
+                    tmp += " ...";
+                }
+
+                light_text.Text = tmp;
+            }
         }
 
         private void Button_Click_Run(object sender, RoutedEventArgs e)
@@ -71,16 +119,10 @@ namespace AstroPhotometry.View
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Multiselect = true;
             openFileDialog.Filter = "Fits files (*.fits)|*.fit;*.fits|All files (*.*)|*.*";
+            openFileDialog.Title = "Open fits files";
+
             if (openFileDialog.ShowDialog() == true)
             {
-                // debug
-                string txt = "";
-                foreach (string file in openFileDialog.FileNames)
-                {
-                    txt += " " + file;
-                }
-                MessageBox.Show(txt);
-                //
                 return openFileDialog.FileNames;
             }
             else
